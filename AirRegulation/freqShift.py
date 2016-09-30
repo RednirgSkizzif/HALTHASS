fe716724b13952cf733101da4c12f4fb8ee596b0
Dylan Frizzell <dylan.frizzell@ou.edu>
2016-09-30 12:29:54 -0500

    Added the variable frequency code
    There now exists two new files. varFreq.ino is currently running on the arduino that controls the cylinder mosfets. This code will run TWO cylinders and can be adapted to more. It constantly scans the serial port for a new timing variable.
    
    The feqShift.py file is a class that has a changeFreq(int frequency) method. This method will send a new frequency to the arduino. Can be adapted to change the frequency on the fly.

--- AirRegulation/freqShift.py
+++ AirRegulation/freqShift.py
@@ -2,11 +2,15 @@ import serial
 
 class freqShift(object):
 	
-	def __init__(self,port):
-		self.ard = serial.Serial(port,9600)
-	def changeFreq(f):
+	def __init__(self,port,baud):
+		self.ard = serial.Serial(port,baud)
+	def changeFreq(self,f):
+		if (f>50):
+			print "TOFAST"
+			return
+
 		deltaT = 1000/f
-		self.ard.write(deltaT
+		self.ard.write(str(deltaT))
 	def close(self):
 		self.ard.close()
 
