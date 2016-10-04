a8bc0d16875801ff3497107fc4316a2cfb3e0ae8
NGrieser <nathan.a.grieser-1@ou.edu>
2016-10-03 19:46:20 -0500

    Combined the classes into VibrationTest class

--- AirRegulation/PropAir.py
+++ AirRegulation/PropAir.py
@@ -1,25 +1,43 @@
 import serial 
 import minimalmodbus as mm
 
-class PropAir(object):
+class VibrationTest(object)
+	def __init__(self):
+		pass
 	
-	def __init__(self,port,stepsize,steplength,number of steps):
-		self.ins = mm.Instrument(port,247)
-		self.ins.serial.parity = 'E'
-		self.ins.serial.timeout = .160
-		self.step_size = step_size
-		self.step_length = step_length
-		self.number_of_steps = number_of_steps
+class Cylinders(VibrationTest):
+	def __init__(self,port,baud):
+		self.ard = serial.Serial(port,baud)
+	def changeFreq(self,f):
+		if (f>50):
+			print "TOFAST"
+			return
 
-	def setPressure(self,pressure):
-		#mapping to press needs to be fixed
-		val = pressure*655
-		self.ins.write_register(49,val)
+		deltaT = 1000/f
+		self.ard.write(str(deltaT))
+	def close(self):
+		self.ard.close()
+
+
+class PropAir(VibrationTest):
 	
-	def readPressure(self):
-		return self.ins.read_register(49)/655
+		def __init__(self,port,stepsize,steplength,number of steps):
+			self.ins = mm.Instrument(port,247)
+			self.ins.serial.parity = 'E'
+			self.ins.serial.timeout = .160
+			self.step_size = step_size
+			self.step_length = step_length
+			self.number_of_steps = number_of_steps
+
+		def setPressure(self,pressure):
+			#mapping to press needs to be fixed
+			val = pressure*655
+			self.ins.write_register(49,val)
+	
+		def readPressure(self):
+			return self.ins.read_register(49)/655
 		
 	
-	def checkGrms(self)
-		# read grms from arduino
-		#return grms
+		def checkGrms(self)
+			# read grms from arduino
+			#return grms
