257ac1e7fe3cf073f77e79aa612274651d8f3782
Dylan Frizzell <dylan.frizzell@ou.edu>
2016-10-06 20:24:04 -0500

    Added grmsRead functionality.
    The constructor now requires the inclusion of an arduino that can input the grms. The function readgrms can return this value.

--- AirRegulation/PropAir.py
+++ AirRegulation/PropAir.py
@@ -19,13 +19,16 @@ class Cylinders(object):
 
 class PropAir(object):
 	
-		def __init__(self,PAport,step_size,step_length,number_of_steps):
+		def __init__(self,PAport,step_size,step_length,number_of_steps,grmsArduinoPort):
 			self.ins = mm.Instrument(PAport,247)
 			self.ins.serial.parity = 'E'
 			self.ins.serial.timeout = .160
 			self.step_size = step_size #This should be given in grms
 			self.step_length = step_length #This should be given in minutes
 			self.number_of_steps = number_of_steps
+			#Lines added by Dylan for the grms read
+			self.rmsReader = serial.Serial(grmsArduinoPort,9600)
+
 
 		def setPressure(self,pressure):
 			#mapping to press needs to be fixed
@@ -36,7 +39,9 @@ class PropAir(object):
 			return self.ins.read_register(49)/655
 		
 	
-		def checkGrms(self)
+		def checkGrms(self):
+			self.rmsReader.reset_input_buffer()
+			return float(self.rmsReader.read())
 			# read grms from arduino
 			#return grms
 
