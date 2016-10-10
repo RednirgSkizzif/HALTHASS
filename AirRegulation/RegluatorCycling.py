0985ac20232660caedaf4f5136a14758ec454121
Lab350 <Lab350>
2016-10-10 11:30:47 -0500

    Changes to checkGrms and loop script
    I have tested the checkGrms function and added a failsafe that will neglect data fragments.
    This also has been tested to take the most recent data from the buffer.
    I also edited a few lines in the looping script. Just declaring an object and changeing a few method calls to proper syntax.

---
+++ AirRegulation/RegluatorCycling.py
@@ -0,0 +1,46 @@
+
+
+import Vibration
+import time
+
+test = Vibration.VibrationTest('COM2',10,5,5,'COM10',9600,grmsPort='COM4')
+
+
+for n in range(0,number_of_steps)
+	
+	t_end = time.time() + 60 * n * step_length
+	while time.time() < t_end:
+
+		
+
+		test.checkGrms() = grms
+		print grms
+		if(grms < (n*step_size - 1) && grms > (n*step_size-3)):
+			pressure = pressure + 1
+			test.setPressure(pressure)
+		else if(grms > (n*step_size+1) && grms < (n*step_size+3)):
+			pressure = pressure - 1
+			test.setPressure(pressure)
+		else if(grms > (n*step_size +3)):
+			pressure = pressure - 3
+			test.setPressure(pressure)
+		else if(grms < n*step_size-3)):
+			pressure = pressure + 3
+			test.setPressure(pressure)
+				
+		test.checkGrms = grms
+		while (grms >= (n * step_size - 1)) && (grms <= (n * step_size + 1):
+			time.sleep(5)
+			test.checkGrms = grms
+			print grms
+			if time.time() >= t_end:
+				break
+	
+		
+		
+
+
+	
+
+
+
--- AirRegulation/Vibration.py
+++ AirRegulation/Vibration.py
@@ -1,6 +1,6 @@
 import serial 
 import minimalmodbus as mm
-
+import time
 
 	
 class Cylinders(object):
@@ -23,12 +23,14 @@ class PropAir(object):
 			self.ins = mm.Instrument(PAport,247)
 			self.ins.serial.parity = 'E'
 			self.ins.serial.timeout = .160
+			self.ins.serial.baudrate = 19200
 			self.step_size = step_size #This should be given in grms
 			self.step_length = step_length #This should be given in minutes
 			self.number_of_steps = number_of_steps
 			#Lines added by Dylan for the grms read
 			if grmsArduinoPort != 'none' :
 				self.rmsReader = serial.Serial(grmsArduinoPort,9600)
+				self.rmsReader.timeout = 2
 
 
 		def setPressure(self,pressure):
@@ -41,14 +43,20 @@ class PropAir(object):
 		
 	
 		def checkGrms(self):
+			#time.sleep(0.5)
+			try:
+			 dum = float(self.rmsReader.readline().strip())
+			except ValueError:
+			 print("Data Fragment Dropped")
+			val = float(self.rmsReader.readline().strip())
 			self.rmsReader.reset_input_buffer()
-			return float(self.rmsReader.read())
+			return val
 			# read grms from arduino
 			#return grms
 
 			
 class VibrationTest(PropAir, Cylinders):
-	def __init__(self, PAport, stepsize, steplength, number_of_steps, Cport, Cbaud):
-		PropAir.__init__(self, PAport, stepsize, steplength, number_of_steps)
+	def __init__(self, PAport, stepsize, steplength, number_of_steps, Cport, Cbaud,grmsPort='none'):
+		PropAir.__init__(self, PAport, stepsize, steplength, number_of_steps,grmsArduinoPort=grmsPort)
 		Cylinders.__init__(self, Cport, Cbaud)
 		
