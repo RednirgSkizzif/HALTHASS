eb7ed1ebcba5005157b48d727621656f857b5dc4
Lab350 <Lab350>
2016-10-11 17:42:09 -0500

    Rename cycling

--- AirRegulation/RegulatorCycling.py
+++ AirRegulation/RegulatorCycling.py
@@ -6,7 +6,7 @@ import time
 test = Vibration.VibrationTest('COM2',10,5,5,'COM10',9600,grmsPort='COM4')
 
 
-for n in range(0,number_of_steps)
+for n in range(1,number_of_steps):
 	
 	t_end = time.time() + 60 * n * step_length
 	while time.time() < t_end:
@@ -15,21 +15,21 @@ for n in range(0,number_of_steps)
 
 		test.checkGrms() = grms
 		print grms
-		if(grms < (n*step_size - 1) && grms > (n*step_size-3)):
+		if(grms < (n*step_size - 1) and grms > (n*step_size-3)):
 			pressure = pressure + 1
 			test.setPressure(pressure)
-		else if(grms > (n*step_size+1) && grms < (n*step_size+3)):
+		elif(grms > (n*step_size+1)  and grms < (n*step_size+3)):
 			pressure = pressure - 1
 			test.setPressure(pressure)
-		else if(grms > (n*step_size +3)):
+		elif(grms > (n*step_size +3)):
 			pressure = pressure - 3
 			test.setPressure(pressure)
-		else if(grms < n*step_size-3)):
+		elif(grms < n*step_size-3):
 			pressure = pressure + 3
 			test.setPressure(pressure)
 				
 		test.checkGrms = grms
-		while (grms >= (n * step_size - 1)) && (grms <= (n * step_size + 1):
+		while (grms >= (n * step_size - 1)) and (grms <= (n * step_size + 1)):
 			time.sleep(5)
 			test.checkGrms = grms
 			print grms
